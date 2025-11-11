d0689eabfd63df419cabe9652a21b15de331b1c8
Maria Vitoria <mvsilvasoaress@gmail.com>
2025-11-11 20:11:11 -0300

    Inicializa projeto Spring Boot com configuração básica

---
+++ backend/src/main/java/com/mvsilvass/dailylist/DailylistApplication.java
@@ -0,0 +1,13 @@
+package com.mvsilvass.dailylist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailylistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailylistApplication.class, args);
+	}
+
+}
