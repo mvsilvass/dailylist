7ababe1408a548a3ca7d40214b9a8c63706789e1
Maria Vitoria <mvsilvasoaress@gmail.com>
2025-11-12 19:35:57 -0300

    adiciona método findByEmail no UserRepository

--- backend/src/main/java/com/mvsilvass/dailylist/repository/UserRepository.java
+++ backend/src/main/java/com/mvsilvass/dailylist/repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.mvsilvass.dailylist.repository;
 import com.mvsilvass.dailylist.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<Long, User>{
+    Optional<User> findByEmail(String email);
 }
