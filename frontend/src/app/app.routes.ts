119c292f80160bab1b2efd852908f2070903fd59
Maria Vitoria <mvsilvasoaress@gmail.com>
2025-12-13 10:38:48 -0300

    Update app.routes.ts

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Login } from './authentication/login/login';
+import { Home } from './features/home/home';
+
+export const routes: Routes = [
+  {
+    title: 'DailyList - Home',
+    path: 'home',
+    component: Home,
+  },
+  {
+    title: 'DailyList - Login',
+    path: 'auth/login',
+    component: Login,
+  },
+];
