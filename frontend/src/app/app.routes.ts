b6271f8a771041bacf716b728a1e87c546a8e870
Maria Vitoria <mvsilvasoaress@gmail.com>
2025-12-14 15:45:36 -0300

    refactor: reorganiza estrutura do projeto

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 
 import { Login } from './authentication/login/login';
-import { Home } from './features/home/home';
+import { Home } from './features/home/pages/home/home';
 
 export const routes: Routes = [
   {
