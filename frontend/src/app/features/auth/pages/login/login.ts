35a11fec8adac890551444f26bfc44ede80d6e75
Maria Vitoria <mvsilvasoaress@gmail.com>
2025-12-15 20:48:36 -0300

    Adiciona serviço de autenticação para login

--- frontend/src/app/features/auth/pages/login/login.ts
+++ frontend/src/app/features/auth/pages/login/login.ts
@@ -1,9 +1,38 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { LoginRequest } from '../../models/login-request';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+
+export class Login {
+  constructor(private authService: AuthService) {}
+
+  loginForm: FormGroup = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+  });
+
+  onSubmit(){
+    const formValue = this.loginForm.value;
+
+    const request: LoginRequest = {
+      email: formValue.email,
+      password: formValue.password,
+    };
+
+    this.authService.doLogin(request).subscribe({
+      next: (loginResponse) => {
+        console.log('login ok', loginResponse);
+      },
+      error: (loginResponse) => {
+        console.log('erro login', loginResponse.error);
+      },
+    });
+  }
+}
---
+++ frontend/src/app/features/auth/services/auth.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { LoginRequest } from '../models/login-request';
+import { Observable } from 'rxjs';
+import { LoginResponse } from '../models/login-response';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  constructor(private http: HttpClient) {}
+
+  doLogin(request: LoginRequest): Observable<LoginResponse> {
+    return this.http.post<LoginResponse>(`${environment.apiUrl}/auth/login`, request);
+  }
+}
