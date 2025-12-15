d481116f929c642dd4ef18aa7da2b2e47ec0068c
Maria Vitoria <mvsilvasoaress@gmail.com>
2025-12-15 20:45:51 -0300

    Update CorsConfig.java

--- backend/src/main/java/com/mvsilvass/dailylist/config/CorsConfig.java
+++ backend/src/main/java/com/mvsilvass/dailylist/config/CorsConfig.java
@@ -1,7 +1,6 @@
 package com.mvsilvass.dailylist.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
