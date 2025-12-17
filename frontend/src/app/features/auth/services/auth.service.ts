7b06f3fb07dd64356a954867de8e7cc91f5583f2
Maria Vitoria <mvsilvasoaress@gmail.com>
2025-12-17 16:56:19 -0300

    Update auth.service.ts

--- frontend/src/app/features/auth/services/auth.service.ts
+++ frontend/src/app/features/auth/services/auth.service.ts
@@ -4,6 +4,8 @@ import { LoginRequest } from '../models/login-request';
 import { Observable } from 'rxjs';
 import { LoginResponse } from '../models/login-response';
 import { environment } from 'src/environments/environment';
+import { RegisterRequest } from '../models/register-request';
+import { RegisterResponse } from '../models/register-response';
 
 @Injectable({
   providedIn: 'root',
@@ -13,5 +15,9 @@ export class AuthService {
 
   doLogin(request: LoginRequest): Observable<LoginResponse> {
     return this.http.post<LoginResponse>(`${environment.apiUrl}/auth/login`, request);
+  }
+
+  doRegister(request: RegisterRequest): Observable<RegisterResponse> {
+    return this.http.post<RegisterResponse>(`${environment.apiUrl}/auth/register`, request);
   }
 }
