c77319ec7d592ef60a448f1da275c8b075314c70
Maria Vitoria <mvsilvasoaress@gmail.com>
2025-12-17 19:37:31 -0300

    Update UserDetailsServiceImpl.java

--- backend/src/main/java/com/mvsilvass/dailylist/security/UserDetailsServiceImpl.java
+++ backend/src/main/java/com/mvsilvass/dailylist/security/UserDetailsServiceImpl.java
@@ -19,7 +19,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        return userRepository.findByEmail(email)
+        return userRepository.findByEmailIgnoreCase(email)
             .map(UserAuthenticated::new)
             .orElseThrow(
                 () -> new UsernameNotFoundException("Usuário não encontrado"));
