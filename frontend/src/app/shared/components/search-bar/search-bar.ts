5656ca60428d3ac8a36c492d310b8550d06d286d
Maria Vitoria <mvsilvasoaress@gmail.com>
2026-01-07 20:36:15 -0300

    Adiciona search-bar component

---
+++ frontend/src/app/shared/components/search-bar/search-bar.ts
@@ -0,0 +1,17 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-search-bar',
+  imports: [],
+  templateUrl: './search-bar.html',
+  styleUrl: './search-bar.css',
+})
+export class SearchBar {
+  @Input() placeholder = 'Pesquisar';
+  @Output() search = new EventEmitter<string>();
+
+  onSearch(event: Event) {
+    const value = (event.target as HTMLInputElement).value;
+    this.search.emit(value);
+  }
+}
