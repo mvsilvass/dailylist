2396ca9e8537a716022ef1fc289f142945f43b12
Maria Vitoria <mvsilvasoaress@gmail.com>
2026-01-08 14:46:16 -0300

    Update user-management.ts

--- frontend/src/app/features/admin/pages/user-management/user-management.ts
+++ frontend/src/app/features/admin/pages/user-management/user-management.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { SearchBar } from "src/app/shared/components/search-bar/search-bar";
 
 @Component({
   selector: 'app-user-management',
-  imports: [],
+  imports: [SearchBar],
   templateUrl: './user-management.html',
   styleUrl: './user-management.css',
 })
 export class UserManagement {
+
+  onSearch($event: string) {
+    alert($event);
+  }
 
 }
