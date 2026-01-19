b178a7ccfef6b37849bc46887a6a55ada47a7fc8
Maria Vitoria <mvsilvasoaress@gmail.com>
2026-01-19 20:46:16 -0300

    Update app.routes.ts

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,11 +1,29 @@
 import { Routes } from '@angular/router';
 import { Home } from './features/home/home';
+import { Login } from './auth/pages/login/login';
+import { Register } from './auth/pages/register/register';
+import { loginGuard } from '@core/guards/login-guard';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
   {
     title: 'DailyList - Home',
     path: 'home',
     component: Home,
+  },
+  {
+    title: 'DailyList - Login',
+    path: 'auth/login',
+    component: Login,
+    canActivate: [loginGuard],
+  },
+  {
+    title: 'DailyList - Register',
+    path: 'auth/register',
+    component: Register,
   },
 ];
