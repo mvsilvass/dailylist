4b65617a67a89af5131d054a9ba858d2816945de
Maria Vitoria <mvsilvasoaress@gmail.com>
2026-01-24 16:43:58 -0300

    Adiciona logos das redes sociais no rodapé

--- frontend/src/app/shared/components/footer/footer.ts
+++ frontend/src/app/shared/components/footer/footer.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrl: './footer.css',
 })
 export class Footer {
-
+  socials = [
+    { name: 'Instagram', icon: 'instagram.svg', href: '#' },
+    { name: 'X-Twitter', icon: 'x-twitter.svg', href: '#' },
+    { name: 'Facebook', icon: 'facebook.svg', href: '#' },
+  ];
 }
