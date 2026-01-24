9efd6fca7eea83ec3318870a79a899af01c66ac8
Maria Vitoria <mvsilvasoaress@gmail.com>
2026-01-24 18:09:45 -0300

    Melhora estrutura do footer

--- frontend/src/app/shared/components/footer/footer.ts
+++ frontend/src/app/shared/components/footer/footer.ts
@@ -6,9 +6,13 @@ import { Component } from '@angular/core';
   styleUrl: './footer.css',
 })
 export class Footer {
+  currentYear: number = new Date().getFullYear();
+  githubUrl = 'https://github.com/mvsilvass';
+
   socials = [
     { name: 'Instagram', icon: 'instagram.svg', href: '#' },
     { name: 'X-Twitter', icon: 'x-twitter.svg', href: '#' },
+    { name: 'Github', icon: 'github.svg', href: this.githubUrl },
     { name: 'Facebook', icon: 'facebook.svg', href: '#' },
   ];
 }
