43a6a0dde4a2d642ef5975564380648341a57bb6
Maria Vitoria <mvsilvasoaress@gmail.com>
2026-01-24 15:11:19 -0300

    Cria componente Header

--- frontend/src/app/features/home/home.ts
+++ frontend/src/app/features/home/home.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { Header } from "app/shared/components/header/header";
+import { Footer } from "app/shared/components/footer/footer";
 
 @Component({
   selector: 'app-home',
-  imports: [Header],
+  imports: [Header, Footer],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
