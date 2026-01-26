130605caa75db0d3852662e016651f0b2bd10427
Maria Vitoria <mvsilvasoaress@gmail.com>
2026-01-26 19:37:17 -0300

    Update task-board.ts

--- frontend/src/app/features/tasks/pages/task-board/task-board.ts
+++ frontend/src/app/features/tasks/pages/task-board/task-board.ts
@@ -1,16 +1,19 @@
 import { Component } from '@angular/core';
+import { IconButton } from "app/shared/components/icon-button/icon-button";
+import { capitalize } from 'app/shared/utils/string.utils';
 
 @Component({
   selector: 'app-task-board',
   templateUrl: './task-board.html',
   styleUrl: './task-board.css',
+  imports: [IconButton],
 })
 export class TaskBoard {
 
   currentDate = new Date();
 
   get currentMonth(): string {
-    return this.currentDate.toLocaleString('pt-BR', { month: 'long' });
+    return capitalize(this.currentDate.toLocaleString('pt-BR', { month: 'long' }));
   }
 
   get currentYear(): number {
