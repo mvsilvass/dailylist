125de5813472d251be274daa8b8465a9c7359767
Maria Vitoria <mvsilvasoaress@gmail.com>
2026-01-27 21:44:04 -0300

    Adiciona campos de data e ajusta campos opcionais e obrigatórios da Task

--- frontend/src/app/features/tasks/models/task.model.ts
+++ frontend/src/app/features/tasks/models/task.model.ts
@@ -1,6 +1,9 @@
 export interface Task {
+  id: number;
   title: string;
-  description: string;
-  link: string;
-  image:string;
+  description?: string;
+  link?: string;
+  image?: string;
+  createdAt: number;
+  updatedAt: number;
 }
