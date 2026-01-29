9e8bcdcb165a5de5db5ac8dc1517baaa6fb6bbef
Maria Vitoria <mvsilvasoaress@gmail.com>
2026-01-29 20:13:58 -0300

    Cria componente TaskColumn

---
+++ frontend/src/app/features/tasks/components/task-column/task-column.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-column',
+  standalone: true,
+  templateUrl: './task-column.component.html',
+  styleUrl: './task-column.component.css',
+})
+export class TaskColumnComponent {
+
+}
