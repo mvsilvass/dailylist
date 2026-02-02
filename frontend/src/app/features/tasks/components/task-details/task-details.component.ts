5cbfff942b41f2daeee7f919f72b48ec4b86a2b5
Maria Vitoria <mvsilvasoaress@gmail.com>
2026-02-01 21:27:35 -0300

    Cria TaskDetailsComponent

---
+++ frontend/src/app/features/tasks/components/task-details/task-details.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+import { Task } from '../../models/task.model';
+
+@Component({
+  selector: 'app-task-details',
+  standalone: true,
+  templateUrl: './task-details.component.html',
+  styleUrl: './task-details.component.css',
+})
+export class TaskDetailsComponent {}
