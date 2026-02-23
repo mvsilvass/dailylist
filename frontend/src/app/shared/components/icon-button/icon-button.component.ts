5312e592988966b9ed1c83dd95812ec4c40806d7
Maria Vitoria <mvsilvasoaress@gmail.com>
2026-02-23 15:39:53 -0300

    refactor: substitui material-icons pelo mat-icon do Angular

--- frontend/src/app/shared/components/icon-button/icon-button.component.ts
+++ frontend/src/app/shared/components/icon-button/icon-button.component.ts
@@ -1,17 +1,19 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-icon-button',
   standalone: true,
   templateUrl: './icon-button.component.html',
   styleUrl: './icon-button.component.css',
+  imports: [MatIconModule],
 })
 export class IconButtonComponent {
   @Input({ required: true }) icon!: string;
   @Input({ required: true }) ariaLabel!: string;
   @Output() clicked = new EventEmitter<void>();
 
-  onClick() {
+  protected onClick() {
     this.clicked.emit();
   }
 }
